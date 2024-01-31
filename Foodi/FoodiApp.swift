e5189031161f197397558f8d4e9fe1ec6f37ae0e
JackRobinson1234 <Jtrobinsonhb@gmail.com>
2024-01-31 21:06:20 +1100

    Initial Commit

---
+++ Foodi/FoodiApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodiApp.swift
+//  Foodi
+//
+//  Created by Jack Robinson on 1/31/24.
+//
+
+import SwiftUI
+
+@main
+struct FoodiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
