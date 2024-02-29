e779c66b60910d89c5b39d44cec5de8dab218b70
JackRobinson1234 <Jtrobinsonhb@gmail.com>
2024-03-01 09:06:57 +1100

    Map Camera Views + viewmodel fixed

--- Foodi/Core/Map/View/MapView.swift
+++ Foodi/Core/Map/View/MapView.swift
@@ -18,6 +18,7 @@ struct MapView: View {
     @State private var isSearchPresented: Bool = false
     @State private var isFiltersPresented: Bool = false
     @ObservedObject var locationManager = LocationManager.shared
+    @State var isLoading = true
     @Namespace var mapScope
 
     
@@ -28,128 +29,136 @@ struct MapView: View {
         self._position = State(initialValue: .userLocation(fallback: .automatic))
         }
     
-    var restaurants: [Restaurant] {
-        Task {await viewModel.fetchRestaurants() }
-        return  viewModel.restaurants
-    }
+    
     var body: some View {
-        NavigationStack{
-            ZStack(alignment: .bottom) {
-                Map(position: $position, selection: $selectedRestaurant, scope: mapScope) {
-                    if !inSearchView{
-                        ForEach(restaurants, id: \.self) { restaurant in
-                            if let coordinates = restaurant.coordinates {
-                                Annotation(restaurant.name, coordinate: coordinates) {
-                                    RestaurantCircularProfileImageView(imageUrl: restaurant.profileImageUrl, color: .blue, size: .medium)
-                                }
-                            }
-                        }
-                    } else {
-                        ForEach(viewModel.searchPreview, id: \.self) { restaurant in
-                            if let coordinates = restaurant.coordinates {
-                                Annotation(restaurant.name, coordinate: coordinates) {
-                                    RestaurantCircularProfileImageView(imageUrl: restaurant.profileImageUrl, color: .blue, size: .medium)
-                                }
-                            }
-                        }
+        if isLoading {
+            // Loading screen
+            ProgressView("Loading...")
+                .onAppear {
+                    Task {
+                        await viewModel.fetchRestaurants()
+                        isLoading = false
                     }
-                    UserAnnotation()
                 }
-                .overlay(alignment: .bottomTrailing) {
+        } else {
+            NavigationStack{
+                ZStack(alignment: .bottom) {
+                    Map(position: $position, selection: $selectedRestaurant, scope: mapScope) {
+                        if !inSearchView{
+                            ForEach(viewModel.restaurants, id: \.self) { restaurant in
+                                if let coordinates = restaurant.coordinates {
+                                    Annotation(restaurant.name, coordinate: coordinates) {
+                                        RestaurantCircularProfileImageView(imageUrl: restaurant.profileImageUrl, color: .blue, size: .medium)
+                                    }
+                                }
+                            }
+                        } else {
+                            ForEach(viewModel.searchPreview, id: \.self) { restaurant in
+                                if let coordinates = restaurant.coordinates {
+                                    Annotation(restaurant.name, coordinate: coordinates) {
+                                        RestaurantCircularProfileImageView(imageUrl: restaurant.profileImageUrl, color: .blue, size: .medium)
+                                    }
+                                }
+                            }
+                        }
+                        UserAnnotation()
+                    }
+                    .overlay(alignment: .bottomTrailing) {
+                        VStack {
+                            MapUserLocationButton(scope: mapScope)
+                        }
+                        .padding([.bottom, .trailing], 20)
+                        .buttonBorderShape(.circle)
+                    }
+                    .mapScope(mapScope)
+                    
                     VStack {
-                        MapUserLocationButton(scope: mapScope)
-                    }
-                    .padding([.bottom, .trailing], 20)
-                    .buttonBorderShape(.circle)
-                }
-                .mapScope(mapScope)
-                
-                VStack {
-                    if !inSearchView{
-                        HStack {
-                            Button(action: {
-                                inSearchView.toggle()
-                                isSearchPresented.toggle()
-                                position = .automatic
-                            }) {
-                                Image(systemName: "magnifyingglass")
-                                    .foregroundStyle(.white)
-                                    .font(.system(size: 27))
-                                    .shadow(color: .gray, radius: 10)
-                                
-                                
-                            }
-                            Spacer()
-                            
-                                .padding(.top)
-                            Button {
-                                isFiltersPresented.toggle()
-                            }
-                        label: {
-                            Image(systemName: "slider.horizontal.3")
-                                .imageScale(.large)
-                                .shadow(radius: 10)
-                                .font(.system(size: 23))
-                            
-                        }
-                        }
-                        .padding(32)
-                        .padding(.top, 20)
-                        .foregroundStyle(.white)
-                        Spacer()
-                    }
-                    else {
-                        VStack{
-                            HStack{
-                                Button{
-                                    inSearchView = false
-                                    position = .userLocation(fallback: .automatic)
-                                } label: {
-                                    Text("Cancel")
-                                        .foregroundStyle(.blue)
-                                        .bold()
+                        if !inSearchView{
+                            HStack {
+                                Button(action: {
+                                    inSearchView.toggle()
+                                    isSearchPresented.toggle()
+                                    position = .automatic
+                                }) {
+                                    Image(systemName: "magnifyingglass")
+                                        .foregroundStyle(.white)
+                                        .font(.system(size: 27))
+                                        .shadow(color: .gray, radius: 10)
+                                    
+                                    
                                 }
                                 Spacer()
+                                
+                                    .padding(.top)
+                                Button {
+                                    isFiltersPresented.toggle()
+                                }
+                            label: {
+                                Image(systemName: "slider.horizontal.3")
+                                    .imageScale(.large)
+                                    .shadow(radius: 10)
+                                    .font(.system(size: 23))
+                                
+                            }
                             }
                             .padding(32)
                             .padding(.top, 20)
+                            .foregroundStyle(.white)
                             Spacer()
+                        }
+                        else {
+                            VStack{
+                                HStack{
+                                    Button{
+                                        inSearchView = false
+                                        position = .userLocation(fallback: .automatic)
+                                    } label: {
+                                        Text("Cancel")
+                                            .foregroundStyle(.blue)
+                                            .bold()
+                                    }
+                                    Spacer()
+                                }
+                                .padding(32)
+                                .padding(.top, 20)
+                                Spacer()
+                            }
+                        }
+                    }
+                    .onChange(of: selectedRestaurant, { oldValue, newValue in
+                        showRestaurantPreview = newValue != nil
+                    })
+                    
+                    
+                    .sheet(isPresented: $isSearchPresented) {
+                        NavigationStack {
+                            MapSearchView(restaurantService: RestaurantService(), mapViewModel: viewModel, inSearchView: $inSearchView)
+                        }
+                    }
+                    .sheet(isPresented: $isFiltersPresented) {
+                        NavigationStack {
+                            FiltersView()
+                        }
+                    }
+                    .mapStyle(.standard(elevation: .realistic))
+                    .clipShape(RoundedRectangle(cornerRadius: 12))
+                    .ignoresSafeArea()
+                    
+                    if showRestaurantPreview, let restaurant = selectedRestaurant {
+                        withAnimation(.snappy) {
+                            MapRestaurantView(restaurant: restaurant)
+                                .onTapGesture {
+                                    showRestaurantPreview.toggle()
+                                    showDetails.toggle()
+                                }
+                            
                         }
                     }
                 }
-                .onChange(of: selectedRestaurant, { oldValue, newValue in
-                    showRestaurantPreview = newValue != nil
-                })
-                
-                
-                .sheet(isPresented: $isSearchPresented) {
-                    NavigationStack {
-                        MapSearchView(restaurantService: RestaurantService(), mapViewModel: viewModel, inSearchView: $inSearchView)
-                    }
-                }
-                .sheet(isPresented: $isFiltersPresented) {
-                    NavigationStack {
-                        FiltersView()
-                    }
-                }
-                .mapStyle(.standard(elevation: .realistic))
-                .clipShape(RoundedRectangle(cornerRadius: 12))
-                .ignoresSafeArea()
-                
-                if showRestaurantPreview, let restaurant = selectedRestaurant {
-                    withAnimation(.snappy) {
-                        MapRestaurantView(restaurant: restaurant)
-                            .onTapGesture {
-                                showRestaurantPreview.toggle()
-                                showDetails.toggle()
-                            }
-                        
-                    }
-                }
+                .onAppear{LocationManager.shared.requestLocation()}
             }
-            .onAppear{LocationManager.shared.requestLocation()}
+            
         }
-        
     }
     
     func clearSelectedListing() {
