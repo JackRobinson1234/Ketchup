97dcfa7b8ae26f12a893dbc7eee12027bd7d2985
JackRobinson1234 <Jtrobinsonhb@gmail.com>
2024-04-23 17:47:59 -0700

    experiement

--- Foodi/Core/Search/SearchTypes/PostList/PostListViewModel.swift
+++ Foodi/Core/Search/SearchTypes/PostList/PostListViewModel.swift
@@ -15,17 +15,19 @@ final class PostListViewModel: ObservableObject {
     @Published var searchQuery: String = ""
     var hits: PaginatedDataViewModel<AlgoliaHitsPage<Hit<Post>>>
     private var itemsSearcher: HitsSearcher
+
     
     init() {
         let appID: ApplicationID = "74A8XPTT50"
         let apiKey: APIKey = "d7d6db8cc90a900cd8fa87fb302b3448"
         let itemsSearcher = HitsSearcher(appID: appID,
                                          apiKey: apiKey,
-                                         indexName: "posts")
+                                         indexName: "posts")        
         self.itemsSearcher = itemsSearcher
         /*self.itemsSearcher.shouldTriggerSearchForQuery = {
             return $0.query.query != ""
         }*/
+        
         self.searchQuery = ""
         self.hits =  itemsSearcher.paginatedData(of: Hit<Post>.self)
     }
